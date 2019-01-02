90b588e81adc675a157645e01906e5bf8a732158
gossoudarev <igossoudarev@herzen.spb.ru>
2019-01-02 20:05:52 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# x
+
+Репозиторий для быстрого клонирования
+
+```
+mkdir $(date +%Y%m%d_%H%M%S) && cd $_ && git clone https://github.com/GossJS/x.git . && yarn
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const [{ Server: h1 }, x] = [require('http'), require('express')];
+
+const Router = x.Router();
+const PORT = 4321;
+const { log } = console;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const app = x();
+Router
+  .route('/')
+  .get(r => r.res.end('Привет мир!'))
+;
+app
+  .use((r, rs, n) => rs.status(200).set(hu) && n())
+  .use(x.static('.'))
+  .use('/', Router)
+  .use(({ res: r }) => r.status(404).end('Пока нет!'))
+  .use((e, r, rs, n) => rs.status(500).end(`Ошибка: ${e}`))
+  /* .set('view engine', 'pug') */
+  .set('x-powered-by', false)
+;
+h1(app)
+  .listen(process.env.PORT || PORT, () => log(process.pid))
+;
