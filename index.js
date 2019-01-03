a9363dfe7bb0404a34e93c1a7e702ab49ee393e0
Ilya Goss <igossoudarev@herzen.spb.ru>
2019-01-03 05:52:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const [{ Server: h1 }, x] = [require('http'), require('express')];
+const socketIO = require('socket.io'); // сокеты
 
 const Router = x.Router();
-const PORT = 4321;
+const PORT = 2222;
 const { log } = console;
 const hu = { 'Content-Type': 'text/html; charset=utf-8' };
 const app = x();
@@ -16,5 +17,14 @@ app
   .use((e, r, rs, n) => rs.status(500).end(`Ошибка: ${e}`))
   /* .set('view engine', 'pug') */
   .set('x-powered-by', false);
-const s = h1(app)
-  .listen(process.env.PORT || PORT, () => log(process.pid));
+const s = h1(app).listen(process.env.PORT || PORT, () => log(process.pid));
+
+const ws = socketIO(s);
+const cb = d => log(d);
+ws.on('connection', wsock => {
+  log('Новый пользователь!');
+  wsock.emit('serv', 'Добро пожаловать!', cb);
+  wsock.on('msg', msg => wsock.emit('serv', `Вы сказали: ${msg}`, cb));		
+  wsock.on('disconnect', () => log('Пользователь отвалился!'));
+});
+
